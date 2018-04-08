f98bbe08670354975b3bcb9b22efbd20e1d71a4c
Louis Williams (kossy) <kossy@kossy.co.uk>
2018-04-08 20:11:32 +0100

    [polybar] Adjusted padding due to monospace font.

--- polybar/.config/polybar/scripts/get-openvpn-status-minimal.sh
+++ polybar/.config/polybar/scripts/get-openvpn-status-minimal.sh
@@ -18,22 +18,22 @@ if [[ $VPN_SERVICE_STATUS = 'active' ]]; then
 
   if [[ $PING_EXIT_STATUS = '2' ]]; then
     # No network connection, exit status 2 from ping.
-    echo %{F$red} '' %{F-}
+    echo %{F$red}''%{F-}
 
   elif [[ $PING_EXIT_STATUS = '0' ]]; then
     # Network connected, exit status 0 from ping.
-    echo %{F$green} '' %{F-}
+    echo %{F$green}''%{F-}
   fi
 
 elif [[ $VPN_SERVICE_STATUS = 'inactive' ]]; then
   # openvpn-client service is inactive, probably due to it being stopped.
-  echo %{F$red} '' %{F-}
+  echo %{F$red}''%{F-}
 
 elif [[ $VPN_SERVICE_STATUS = 'failed' ]]; then
   # openvpn-client failed due to error. More than likely a config error.
-  echo %{F$red} '' %{F-}
+  echo %{F$red}''%{F-}
 
 else
   # Catch all, just print openvpn-client vpn connection status.
-  echo %{F#$orange} '' %{F-}
+  echo %{F#$orange}''%{F-}
 fi
